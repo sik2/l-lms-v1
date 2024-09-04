77196d20302c1b861308f501be9a6b5f7234fad9
sik2 <bickwang123@gmail.com>
2024-09-05 01:00:20 +0900

    스웨거 설정 파일 등록(#4)

---
+++ src/main/java/lab/loop/lms/global/springDoc/SpringDocConfig.java
@@ -0,0 +1,27 @@
+package lab.loop.lms.global.springDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "l-lms API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SpringDocConfig {
+    @Bean
+    public GroupedOpenApi apiGroupV1() {
+        return GroupedOpenApi.builder()
+                .group("API V1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+}
