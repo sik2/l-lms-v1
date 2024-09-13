426b0eddd8a5a186c35aaaadcf5a2eee662d9471
sik2 <bickwang123@gmail.com>
2024-09-14 01:23:10 +0900

    실습 수정(#12)

--- src/main/java/lab/loop/lms/domain/practice/controller/ApiV1PracticesController.java
+++ src/main/java/lab/loop/lms/domain/practice/controller/ApiV1PracticesController.java
@@ -5,17 +5,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lab.loop.lms.domain.member.entity.Member;
 import lab.loop.lms.domain.practice.dto.PracticeDto;
+import lab.loop.lms.domain.practice.entity.Practice;
 import lab.loop.lms.domain.practice.service.PracticeService;
+import lab.loop.lms.global.rq.Rq;
 import lab.loop.lms.global.rsData.RsData;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -25,6 +27,13 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class ApiV1PracticesController {
 
     private final PracticeService practiceService;
+    private final Rq rq;
+
+    @AllArgsConstructor
+    @Getter
+    public static class CreatePracticeResponse {
+        private final PracticeDto practiceDto;
+    }
 
     @Getter
     @Setter
@@ -46,25 +55,59 @@ public class ApiV1PracticesController {
 
     @PostMapping(value = "")
     @Operation(summary = "실습문제 등록")
-    public RsData<?> createPractice(@Valid @RequestBody CreatePracticeRequest req, BindingResult br) {
+    public RsData<CreatePracticeResponse> createPractice(@Valid @RequestBody CreatePracticeRequest createPracticeRequest, BindingResult bindingResult) {
+        Member member = rq.getMember();
+        RsData<Practice> result = practiceService.create(member, createPracticeRequest.getTitle(), createPracticeRequest.getContent(),  createPracticeRequest.getLangType(), createPracticeRequest.getIsPublic(),);
 
-        if(br.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return RsData.of("400-1","유효하지 않은 입력 입니다.");
         }
 
-        return practiceService.createPractice(req);
+        return RsData.of(
+                result.getResultCode(),
+                result.getMsg(),
+                new CreatePracticeResponse(new PracticeDto(result.getData()))
+        );
+     }
+
+    @AllArgsConstructor
+    @Getter
+    public static class PracticeResponse {
+        private final PracticeDto practiceDto;
     }
 
     @GetMapping(value = "/{id}")
     @Operation(summary = "실습문제 단건 조회")
-    public RsData<PracticeDto> getPracticeById(@PathVariable("id") Long id) {
-        return practiceService.getPracticeById(id);
+    public RsData<PracticeResponse> getPracticeById(@PathVariable("id") Long id) {
+        RsData<PracticeDto> result = practiceService.getPracticeById(id);
+        return RsData.of(
+                result.getResultCode(),
+                result.getResultCode(),
+                new PracticeResponse(result.getData())
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class PracticesResponse {
+        private final List<PracticeDto> practices;
     }
 
     @GetMapping(value = "")
     @Operation(summary = "실습문제 다건 조회")
-    public RsData<List<PracticeDto>> getPractices() {
-        return practiceService.getPractices();
+    public RsData<PracticesResponse> getPractices() {
+        RsData<List<PracticeDto>> result =  practiceService.getPractices();
+        return RsData.of(
+                result.getResultCode(),
+                result.getMsg(),
+                new PracticesResponse(result.getData())
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class UpdatePracticeResponse {
+        private final Practice practice;
     }
 
     @Getter
@@ -88,21 +131,38 @@ public class ApiV1PracticesController {
 
     }
 
-    @DeleteMapping(value = "/{id}")
-    @Operation(summary = "실습문제 삭제")
-    public RsData<?> deletePracticeById(@PathVariable("id") Long id) {
-
-        return practiceService.deletePracticeById(id);
-    }
-
-    @PatchMapping(value = "")
+    @PatchMapping(value = "/{id}")
     @Operation(summary = "실습문제 수정")
-    public RsData<?> updatePractice(@Valid @RequestBody UpdatePracticeRequest req, BindingResult br) {
-
-        if(br.hasErrors()){
-            return RsData.of("400-1","유효하지 않은 입력 입니다.",1);
+    public RsData<UpdatePracticeResponse> updatePractice(@Valid @RequestBody UpdatePracticeRequest updatePracticeRequest, BindingResult bindingResult, @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors()) {
+            return RsData.of("400-1","유효하지 않은 입력 입니다.");
         }
 
-        return practiceService.updatePractice(req);
+        Member member = rq.getMember();
+
+        RsData< Practice> result = practiceService.updatePractice(updatePracticeRequest, member);
+
+        return RsData.of(
+                result.getResultCode(),
+                result.getMsg(),
+                new UpdatePracticeResponse(result.getData())
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class RemovePracticeResponse {
+        private final Practice practice;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "실습문제 삭제")
+    public RsData<RemovePracticeResponse> deletePracticeById(@PathVariable("id") Long id) {
+        RsData<Practice> result = practiceService.deletePracticeById(id);
+        return RsData.of(
+                result.getResultCode(),
+                result.getMsg(),
+                new RemovePracticeResponse(result.getData())
+        );
     }
 }
--- src/main/java/lab/loop/lms/domain/practice/service/PracticeService.java
+++ src/main/java/lab/loop/lms/domain/practice/service/PracticeService.java
@@ -1,5 +1,6 @@
 package lab.loop.lms.domain.practice.service;
 
+import lab.loop.lms.domain.member.entity.Member;
 import lab.loop.lms.domain.practice.controller.ApiV1PracticesController;
 import lab.loop.lms.domain.practice.dto.PracticeDto;
 import lab.loop.lms.domain.practice.entity.Practice;
@@ -19,24 +20,22 @@ public class PracticeService {
     private final PracticeRepository practiceRepository;
     private final Rq rq;
 
-    public RsData<?> createPractice(ApiV1PracticesController.CreatePracticeRequest req) {
+    public RsData<Practice> create(Member member, String title, String content, String langType, boolean isPublic) {
 
-        if(practiceRepository.existsByTitle(req.getTitle())){
-            return RsData.of("400-2", "해당 제목으로 이미 문제가 있습니다.",1);
+        if (practiceRepository.existsByTitle(title)) {
+            return RsData.of("400", "해당 제목으로 이미 실습문제가 있습니다.");
         }
 
-        Practice prc = Practice.builder()
-                .member(rq.getMember())
-                .title(req.getTitle())
-                .content(req.getContent())
-                .langType(req.getLangType())
-                .isPublic(req.getIsPublic())
+        Practice practice = Practice.builder()
+                .member(member)
+                .title(title)
+                .content(content)
+                .langType(langType)
+                .isPublic(isPublic)
                 .build();
+        practiceRepository.save(practice);
 
-        practiceRepository.save(prc);
-        
-        // 현재 RsData의 마지막 파라미터를 빼면 오류, null로 넣어도 오류 나서 일단 1로 넣어둠
-        return RsData.of("200","문제 생성 성공",1);
+        return RsData.of("200","실습문제 생성 성공", practice);
     }
 
     public RsData<PracticeDto> getPracticeById(Long id) {
@@ -50,46 +49,46 @@ public class PracticeService {
 
         List<PracticeDto> practices = practiceRepository.findAll().stream().map(PracticeDto::new).toList();
 
-        return RsData.of("200","문제 상세 조회",practices);
+        return RsData.of("200","문제 상세 조회", practices);
     }
 
-    public RsData<?> deletePracticeById(Long id) {
+    public RsData<Practice> deletePracticeById(Long id) {
 
         Practice practice = practiceRepository.findById(id).orElseThrow(() -> new GlobalException("400-1", "존재하지 않는 문제 입니다."));
 
         if(practice.getMember().getId() != rq.getMember().getId()){
-            return RsData.of("400-2", "권한이 없습니다.",1);
+            return RsData.of("400-2", "권한이 없습니다.");
         }
 
         practiceRepository.delete(practice);
 
-        return RsData.of("200","해당 문제를 삭제 했습니다.",1);
+        return RsData.of("200","해당 문제를 삭제 했습니다.", practice);
     }
 
-    public RsData<?> updatePractice(ApiV1PracticesController.UpdatePracticeRequest req) {
+    public RsData<Practice> updatePractice(ApiV1PracticesController.UpdatePracticeRequest updatePracticeRequest, Member member) {
 
-        Practice practice = practiceRepository.findById(req.getId()).orElseThrow(() -> new GlobalException("400-1", "존재하지 않는 문제 입니다."));
+        Practice practice = practiceRepository.findById(updatePracticeRequest.getId()).orElseThrow(() -> new GlobalException("400-1", "존재하지 않는 문제 입니다."));
 
-        if(practice.getMember().getId() != rq.getMember().getId()){
-            return RsData.of("400-2", "권한이 없습니다.",1);
+        if (practice.getMember().getId() != member.getId()) {
+            return RsData.of("400-2", "권한이 없습니다.");
         }
 
         // 제목 변경시 중복 검사
-        if(!practice.getTitle().equals(req.getTitle())){
-            if(practiceRepository.existsByTitle(req.getTitle())){
-                return RsData.of("400-2", "해당 제목으로 이미 문제가 있습니다.",1);
+        if(!practice.getTitle().equals(updatePracticeRequest.getTitle())){
+            if(practiceRepository.existsByTitle(updatePracticeRequest.getTitle())){
+                return RsData.of("400-2", "해당 제목으로 이미 문제가 있습니다.");
             }
         }
 
         Practice updatePractice = practice.toBuilder()
-                .title(req.getTitle())
-                .content(req.getContent())
-                .langType(req.getLangType())
-                .isPublic(req.getIsPublic())
+                .title(updatePracticeRequest.getTitle())
+                .content(updatePracticeRequest.getContent())
+                .langType(updatePracticeRequest.getLangType())
+                .isPublic(updatePracticeRequest.getIsPublic())
                 .build();
 
         practiceRepository.save(updatePractice);
 
-        return RsData.of("200","해당 문제를 업데이트 했습니다.",new PracticeDto(updatePractice));
+        return RsData.of("200","해당 문제를 업데이트 했습니다.", practice);
     }
 }
